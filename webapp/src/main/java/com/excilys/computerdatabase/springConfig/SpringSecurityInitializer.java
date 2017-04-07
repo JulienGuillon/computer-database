e2d9feac8f84ea348b4c9cb18f2928e41808fa12
Guillon Julien <jguillon@excilys.com>
2017-04-07 18:07:44 +0200

    [UPDATE] change to maven multimodule

---
+++ webapp/src/main/java/com/excilys/computerdatabase/springConfig/SpringSecurityInitializer.java
@@ -0,0 +1,12 @@
+package com.excilys.computerdatabase.springConfig;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author Guillon Julien
+ *
+ * 7 avr. 2017
+ */
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
